94a36ade47c6bd2c18b43fe62b49f4358c95eb4b
Xhanti Mzozoyana <xhantimzozoyana123@gmail.com>
2025-07-14 01:11:04 +0000

    WHy  am I still seening this on my PWA app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ function LandingContent() {
   return (
     <>
       <AuthBackgroundImage />
-      <div className="relative z-10 flex min-h-screen flex-col justify-center p-4">
+      <div className="relative z-10 flex min-h-screen flex-col items-center justify-center p-4">
         <div className="frosted-card mx-auto flex w-full max-w-sm flex-col items-center gap-6 p-8">
             <Image
                 src="https://gallery.scaneats.app/images/ScanEatsLogo.png"
